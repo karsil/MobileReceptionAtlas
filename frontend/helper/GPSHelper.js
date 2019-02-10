25e342f63373a2785db69c7b95f7a4de84184ce4
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2019-02-10 11:10:51 +0100

    renamed getLocation result functions

--- frontend/helper/GPSHelper.js
+++ frontend/helper/GPSHelper.js
@@ -7,21 +7,21 @@ import { updateGPS } from './GPSHelper.Action';
 export const getLocationAsync = async () => {
         let { status } = await Permissions.askAsync(Permissions.LOCATION);
         if (status !== 'granted') {
-            gotLocationError('Permission to access location was denied')
+            getLocationError('Permission to access location was denied')
         }
         else {
             console.log('Permission to access location was granted')
             let location = await Location.getCurrentPositionAsync({});
-            gotLocationSuccess(location)
+            getLocationSuccess(location)
         }
 };
 
-gotLocationSuccess = locationData => {
+getLocationSuccess = locationData => {
     const {latitude, longitude} = locationData.coords
     console.log('setting to x ' + latitude + ' and y ' + longitude)
     store.dispatch(updateGPS(latitude, longitude))
 }
 
-gotLocationError = errorMessage => {
+getLocationError = errorMessage => {
     alert(errorMessage)
 }
