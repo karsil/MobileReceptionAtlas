4855b6074e4a8bf5d87c150b1fb765c924d58db8
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2019-02-11 15:08:19 +0100

    fixed reducer to correct state

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -13,7 +13,7 @@ const initialState = {
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case FETCH_RESULT:
-            return { ...state, array: action.payload.result };
+            return { ...state, data: action.payload.result };
         case SHOW_MAP:
             return {
                 ...state,
